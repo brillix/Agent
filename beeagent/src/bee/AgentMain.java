ddc0dddc2e35b887e353ba70cae16b8729364c10
brillix <baruch@brillix.co.il>
2014-06-06 14:59:16 +0300

    no message

--- beeagent/src/bee/AgentMain.java
+++ beeagent/src/bee/AgentMain.java
@@ -42,14 +42,31 @@ public class AgentMain {
 				e.printStackTrace();
 			}// end try
 			}
-		
+
 	    };
-	    
+	    //--run 
 	    Sched.ScheduleTask(Test, 1, 2);
+	   	    
 		//System.out.print(query1.RunQuery("oracle.jdbc.driver.OracleDriver","select open_mode from v$database","jdbc:oracle:thin:@localhost:1521:xe","system","test123")+"\n");		
 	 
 	//System.out.print(query1.RunQuery("com.mysql.jdbc.Driver","select user,host from user where password_expired = 'N'","jdbc:mysql://192.168.56.200:3306/mysql","root","test123"));
 		//networkConn
+	    
+	    
+	    /*  
+	     * Test mail
+	     *
+	     */
+	    
+	//    String user = "agents@brillix.co.il";
+    //    String pass = "JHDKSHlsmlsdjfLJLKJLJLKJljslfdj12312321";
+      //  String[] to = {"omri@brillix.co.il","oded@brillix.co.il","baruch@brillix.co.il"}; // list of recipient email addresses
+    //    String[] to = {"baruch@dbaces.com","oded@dbaces.com"};
+    //    String subject = "thanks from the dbee agent team :-) dbaces.com ";
+    //    String body = "Welcome to dbee agent from dbaces.com !";
+
+      //  Ncom.sendMail(user,"agents@dbaces.com", pass, to, subject, body);
+
 	 }
 
 }
--- beeagent/src/bee/Ncom.java
+++ beeagent/src/bee/Ncom.java
@@ -1,15 +1,77 @@
 package bee;
 
+/*
+ * by baruch o 
+ * 27.5.2014  create ,send put to web site
+ * 
+ * 
+ */
+
+
 import java.io.OutputStreamWriter;
 import java.net.*;
+//for mail 
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 
 public class Ncom {
+
 	public static void main(String[] args) throws Exception {
 	
 	}
 	
 	
+//send mail test for Gmail	
+public static void sendMail(String user,String from, String pass, String[] to, String subject, String body)
+{
+	   Properties props = System.getProperties();
+       String host = "smtp.gmail.com";
+       props.put("mail.smtp.starttls.enable", "true");
+       props.put("mail.smtp.host", host);
+       props.put("mail.smtp.user", user);
+       props.put("mail.smtp.password", pass);
+       props.put("mail.smtp.port", "587");
+       props.put("mail.smtp.auth", "true");
+       props.put("mail.smtp.from",from);
+
+       Session session = Session.getDefaultInstance(props);
+       MimeMessage message = new MimeMessage(session);
+
+       try {
+           message.setFrom(new InternetAddress(from));
+           InternetAddress[] toAddress = new InternetAddress[to.length];
+
+           // To get the array of addresses
+           for( int i = 0; i < to.length; i++ ) {
+               toAddress[i] = new InternetAddress(to[i]);
+           }
+
+           for( int i = 0; i < toAddress.length; i++) {
+               message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+           }
+
+           message.setSubject(subject);
+           message.setText(body);
+           Transport transport = session.getTransport("smtp");
+           transport.connect(host, user, pass);
+           transport.sendMessage(message, message.getAllRecipients());
+           transport.close();
+       }
+       catch (AddressException ae) {
+           ae.printStackTrace();
+       }
+       catch (MessagingException me) {
+           me.printStackTrace();
+       }    
+
+
+    
+	
+}	
+	
+	// putXML send get to web site
 	public static void putXML(String XmlString,String Url) throws Exception
 	{
 		//"http://192.168.56.200/ptest.php"
